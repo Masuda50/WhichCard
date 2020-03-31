57684755833aab97a76a255cf2e822a01ea4e17f
aqsa505 <aqsamalik70@gmail.com>
2020-03-30 20:53:47 -0400

    Add form

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Which Card</h1>')
+
+def form(request):
+    return render(request, 'cards/cardform.html')
+
