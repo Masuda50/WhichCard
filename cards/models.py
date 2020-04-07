63d4d08b6f17fcc3612865e9c9fd5c91504f4314
Masuda50 <mfarehia@gmail.com>
2020-04-07 09:33:08 -0400

    edited database

--- cards/models.py
+++ cards/models.py
@@ -3,24 +3,26 @@ from django.db import models
 # Create your models here.
 
 class Card (models.Model): 
+	ID = models.IntegerField()
 	cardName=models.CharField(max_length=200)
 	bankName=models.CharField(max_length=200)
 	annualFee= models.IntegerField()
 	rewardsType=models.CharField(max_length=200)
-	rewardValue= models.DecimalField(max_digits=5, decimal_places=5)
+	rewardValue= models.DecimalField(max_digits=10, decimal_places=5)
 	rewardsDisplay= models.TextField()
-	groceryMultiplier= models.IntegerField()
-	restMultiplier= models.IntegerField()
-	travelMultiplier= models.IntegerField()
-	otherMultiplier=models.IntegerField()
-	gasMultiplier= models.IntegerField()
-	valOfSingRe= models.IntegerField()
-	everythingElse = models.IntegerField(default=1)
-	APR= models.IntegerField()
-	bonusDisplay=models.TextField()
-	bonusValue= models.IntegerField() 
+	groceryMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+	restMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+	travelMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+	gasMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+	otherMultiper=models.DecimalField(max_digits=10, decimal_places=5, default = 0)
+	APR= models.DecimalField(max_digits=10, decimal_places=5, default=-1)
+	bonusDisplay=models.TextField(blank=True,null=True)
+	bonusValue= models.IntegerField(default=0) 
 	link=models.TextField()
-	creditScore= models.IntegerField()
+	creditScore= models.CharField(max_length=200)
+
+
+
 
 	def __str__(self):
 		return self.cardName
