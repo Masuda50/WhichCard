f677097a35207de36b4e1b54cd598c572287dc21
Masuda50 <mfarehia@gmail.com>
2020-04-14 17:04:16 -0400

    changed the database to handle decimals, and created a way for the database to be automatically uploade anytime runserver is entered

--- cards/models.py
+++ cards/models.py
@@ -2,27 +2,28 @@ from django.db import models
 
 # Create your models here.
 
-
 class Card (models.Model): 
-	ID = models.IntegerField()
+
 	cardName=models.CharField(max_length=200)
 	bankName=models.CharField(max_length=200)
 	annualFee= models.IntegerField()
 	rewardsType=models.CharField(max_length=200)
 	rewardValue= models.DecimalField(max_digits=10, decimal_places=5)
 	rewardsDisplay= models.TextField()
+	groceryMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+	restMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+	travelMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+	gasMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
 
-	groceryMultiplier= models.IntegerField()
-	restaurantMultiplier= models.IntegerField()
-	travelMultiplier= models.IntegerField()
-	otherMultiplier=models.IntegerField()
-	gasMultiplier= models.IntegerField()
-	everythingElse = models.IntegerField(default=1)
-	APR= models.IntegerField()
-	bonusDisplay=models.TextField()
-	bonusValue= models.IntegerField() 
+	elseMultiper=models.DecimalField(max_digits=10, decimal_places=5, default=0)
+	APR= models.DecimalField(max_digits=10, decimal_places=5, default=0)
+	bonusDisplay=models.TextField(blank=True,null=True)
+
+	bonusValue= models.IntegerField(default=0) 
 	link=models.TextField()
 	creditScore= models.CharField(max_length=200)
+
+
 
 
 	def __str__(self):
