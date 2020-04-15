6364fc1fe683b8795971f330c96b85c05922f05b
NComer98 <NComer98@gmail.com>
2020-04-14 21:42:03 -0400

    Update base_test_class.py
    Chrome options

--- functional_tests/base_test_class.py
+++ functional_tests/base_test_class.py
@@ -1,12 +1,17 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 class BaseFunctionalTest(LiveServerTestCase):
 
     # ran at start of every test
     def setUp(self):
-        self.browser = webdriver.Chrome('functional_tests/chromedriver')
+        self.browser = webdriver.Chrome('functional_tests/chromedriver',chrome_options=chrome_options)
 
     # ran at end of every test
     def tearDown(self):
