e8f72465260529f0b797bcad0f40def6c3607c5f
Natalia <natalia@Roberts-MacBook-Pro.local>
2020-04-15 17:56:46 -0400

    working branch with tests and the form

---
+++ cards/tests/test_models 2.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from ..models import Card
+
+
+class CardModelTest(TestCase):
+
+	# create new card entry for test cases
+	def setUp(self):
+		self.card = Card(
+			cardName="Chase Freedom",
+			bankName="Chase Bank",
+			annualFee=0,
+			rewardsType="Cash Back",
+			rewardValue=0.01,
+			rewardsDisplay="5% cash back on select categories",
+			groceryMultiplier=2,
+			restMultiplier=2,
+			travelMultiplier=2,
+			gasMultiplier=2,
+			elseMultiplier=1,
+			APR=2,
+			bonusDisplay="dd",
+			bonusValue=2,
+			link="fakesite.com",
+			creditScore=2)
+
+		self.card.save()
+
+	# test model str function
+	def test_card_string_function(self):
+		self.assertEqual(self.card.__str__(), self.card.cardName)
---
+++ functional_tests/test_home_page 2.py
@@ -0,0 +1,43 @@
+from django.urls import reverse
+from .base_test_class import BaseFunctionalTest
+
+
+class TestHomePage(BaseFunctionalTest):
+
+    def setUp(self):
+        super().setUp()
+        self.browser.get(self.live_server_url)                 # home url
+
+    # test that the homepage was loaded
+    def test_user_first_visit(self):
+        landing = self.browser.find_element_by_tag_name('body')
+
+        self.assertEqual(
+            landing.text, 'Which Card\nAbout Us\nForms\nHome'
+        )
+
+    # test click Forms in navbar
+    def test_home_page_redirects_to_forms(self):
+        self.__check_navbar_redirection_to("Forms", 'cards:forms')
+
+    # test click About us in navbar
+    def test_home_page_redirects_to_aboutus(self):
+        self.__check_navbar_redirection_to("About Us", 'cards:aboutus')
+
+    # test click Home in navbar
+    def test_home_page_redirects_to_home(self):
+        self.__check_navbar_redirection_to("Home", "cards:home")
+
+    # helper to test navigation bar re-directions
+    def __check_navbar_redirection_to(self, element, destination):
+
+        assert element is not None and not element.isspace()
+        assert destination is not None and not destination.isspace()
+
+        self.browser.find_element_by_link_text(element).click()
+
+        target_url = self.live_server_url + reverse(destination)
+
+        self.assertEquals(self.browser.current_url,
+                          target_url)
+
