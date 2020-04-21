fa6145d3ef7c0b18098621bf76f905ba56a77ab6
Masuda50 <54605733+Masuda50@users.noreply.github.com>
2020-04-20 22:19:42 -0400

    Revert "Postgres integration"

--- functional_tests/test_home_page.py
+++ functional_tests/test_home_page.py
@@ -13,7 +13,7 @@ class TestHomePage(BaseFunctionalTest):
         landing = self.browser.find_element_by_tag_name('body')
 
         self.assertEqual(
-            landing.text, 'Which Card\nAbout Us Forms Home'
+            landing.text, 'Which Card\nAbout Us\nForms\nHome'
         )
 
     # test click Forms in navbar
