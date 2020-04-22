89ad27432d6551f807179889131c7e5829e4a9bf
NComer98 <NComer98@gmail.com>
2020-04-22 17:45:33 -0400

    Production Push (#19)
    * Update settings.py
    
    * Update requirements.txt
    
    * Remove ssl requirement
    
    * UI Change Fix
    
    * Revert "Postgres integration"
    
    * Changed db, fixed view refrences, added automatic data upload, db is postgres on heroku, dbsqlite on local
    
    * dotenv package -> python-dotenv
    
    * Defined default database for sqlite3
    
    * restMultiplier -> restaurantMultiplier
    
    * restMultiplier -> restaurantMultiplier
    
    * Revert
    
    * Revert
    
    * restaurantMultiplier -> restMultiplier
    
    * restaurantMultiplier -> restMultiplier
    
    * everythingElse -> elseMultiplier
    
    * everythingElse -> elseMultiplier
    
    Co-authored-by: Masuda50 <54605733+Masuda50@users.noreply.github.com>
    Co-authored-by: Masuda50 <mfarehia@gmail.com>

--- cards/models.py
+++ cards/models.py
@@ -2,24 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+class Card (models.Model): 
 
-class Card (models.Model):
 	cardName=models.CharField(max_length=200)
 	bankName=models.CharField(max_length=200)
 	annualFee= models.IntegerField()
 	rewardsType=models.CharField(max_length=200)
 	rewardValue= models.DecimalField(max_digits=10, decimal_places=5)
 	rewardsDisplay= models.TextField()
-	groceryMultiplier= models.IntegerField()
-	restaurantMultiplier= models.IntegerField()
-	travelMultiplier= models.IntegerField()
-	gasMultiplier= models.IntegerField()
-	everythingElse = models.IntegerField(default=1)
-	APR= models.IntegerField()
-	bonusDisplay=models.TextField()
-	bonusValue= models.IntegerField() 
+	groceryMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+	restMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+	travelMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+	gasMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
+
+	elseMultiplier=models.DecimalField(max_digits=10, decimal_places=5, default=0)
+	APR= models.DecimalField(max_digits=10, decimal_places=5, default=1)
+	bonusDisplay=models.TextField(blank=True,null=True)
+
+	bonusValue= models.IntegerField(default=1) 
 	link=models.TextField()
 	creditScore= models.CharField(max_length=200)
+
+
+
 
 	def __str__(self):
 		return self.cardName
