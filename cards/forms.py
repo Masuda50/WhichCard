ccb93f64fb4321d919b4b91a903f0f3caf1d07da
jvberdec <44758976+jvberdec@users.noreply.github.com>
2020-04-22 18:24:18 -0400

    front end task #4: additonal info, adding to views for retrieval

--- cards/forms.py
+++ cards/forms.py
@@ -5,6 +5,7 @@ ANNUAL_CHOICES = [('yes', 'Yes'), ('no', 'No')]
 BANK_CHOICES   = [('chase', 'Chase'), ('citi', 'Citibank'), ('amex', 'American Express'), ('capital one', 'Capital One'), ('wells fargo','Wells Fargo'), ('bank of america', 'Bank of America'), ('all', 'All of the Above')]
 
 
+
 class CreditForm(forms.Form):
     groceries = forms.DecimalField(label='Groceries', min_value=0)
     dining = forms.DecimalField(label='Dining out', min_value=0)
@@ -12,5 +13,5 @@ class CreditForm(forms.Form):
     travels = forms.DecimalField(label='Travels', min_value=0)
     etc = forms.DecimalField(label='Everything Else', min_value=0)
     credit_score = forms.CharField(label='What is your credit score?', widget= forms.Select(choices= CREDIT_CHOICES))
-    annual= forms.ChoiceField(label='Are you only interested in cards with no annual fee?', widget= forms.RadioSelect, choices= ANNUAL_CHOICES)
-    # banks = forms.ModelMultipleChoiceField(label= 'Show cards from the following banks:', widget= forms.CheckboxSelectMultiple, choices= BANK_CHOICES)
+    annual= forms.ChoiceField(label='Are you only interested in cards with no annual fee?', widget= forms.RadioSelect, choices= ANNUAL_CHOICES, initial= 'no')
+    banks = forms.MultipleChoiceField(label= 'Show cards from the following banks:', widget= forms.CheckboxSelectMultiple, choices= BANK_CHOICES, initial= 'all')
