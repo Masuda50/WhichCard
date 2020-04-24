bf40e65aafb5d01a30ac289e72699a7a83a839f9
aqsa505 <aqsamalik70@gmail.com>
2020-04-23 22:42:07 -0400

    update-model.py

--- cards/models.py
+++ cards/models.py
@@ -14,18 +14,17 @@ class Card (models.Model):
 	restMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
 	travelMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
 	gasMultiplier= models.DecimalField(max_digits=10, decimal_places=5)
-<<<<<<< HEAD
+
 
 	elseMultiplier=models.DecimalField(max_digits=10, decimal_places=5, default=0)
 	APR= models.DecimalField(max_digits=10, decimal_places=5, default=1)
 	bonusDisplay=models.TextField(blank=True,null=True)
 
-=======
 	elseMultiplier=models.DecimalField(max_digits=10, decimal_places=5, default=0)
 	APR= models.DecimalField(max_digits=10, decimal_places=5, default=1)
 	bonusDisplay=models.TextField(blank=True,null=True)
->>>>>>> origin/dev
-	bonusValue= models.IntegerField(default=1) 
+
+	bonusValue= models.IntegerField(default=1)
 	link=models.TextField()
 	creditScore= models.CharField(max_length=200)
 	bonusMinimumSpend=models.IntegerField()
@@ -37,5 +36,3 @@ class Card (models.Model):
 
 	def __str__(self):
 		return self.cardName
-
-
