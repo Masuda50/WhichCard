613622b788646aadd1a0acc2f54bac784181676f
NComer98 <NComer98@gmail.com>
2020-05-04 18:09:33 -0400

    Production Push (#38)
    * fixed form test bug and form webpage selenium bug
    
    * deleted files
    
    * fixing my conflict
    
    * added unit test for views get cards helpers
    
    * changed header to new theme
    
    * Which Card
    
    * implemented theme to form page
    
    * theme in about us (almost done)
    
    * show top 5 cards
    
    * add apply button next to cards
    
    * index.html file added, minimized forms, aboutus and home
    
    * Styling credit card results
    
    * aboutUs styled
    
    * made submit button match blue theme
    
    * Integer Input
    
    * Decimal form inputs
    
    * Styling
    
    * Added a filter method that would filter the calculated cards based on the new form inputs
    
    * Arithmatic error
    
    * re-add apply button
    
    * fixed cards test bugs
    
    * fixed test bugs associated with ui update
    
    * fix ui test errors
    
    * fix landing text error
    
    * fix redirect error
    
    Co-authored-by: ilroy <yorli0114@gmail.com>
    Co-authored-by: jvberdec <44758976+jvberdec@users.noreply.github.com>
    Co-authored-by: quetourah <dina.qd.academics@gmail.com>
    Co-authored-by: aqsa505 <aqsamalik70@gmail.com>
    Co-authored-by: Masuda50 <mfarehia@gmail.com>

--- cards/forms.py
+++ cards/forms.py
@@ -3,16 +3,15 @@ from django import forms
 
 CREDIT_CHOICES = [('excellent', 'Excellent'), ('good', 'Good'), ('average', 'Average'), ('bad', 'Bad')]
 ANNUAL_CHOICES = [('yes', 'Yes'), ('no', 'No')]
-BANK_CHOICES   = [('chase', 'Chase'), ('citi', 'Citibank'), ('amex', 'American Express'), ('capital one', 'Capital One'), ('wells fargo','Wells Fargo'), ('bank of america', 'Bank of America'), ('all', 'All of the Above')]
+BANK_CHOICES   = [('Chase', 'Chase'), ('Citibank', 'Citibank'), ('American Express', 'American Express'), ('Capital One', 'Capital One'), ('Wells Fargo','Wells Fargo'), ('Bank of America', 'Bank of America'), ('all', 'All of the Above')]
 
 
 class CreditForm(forms.Form):
-
-    groceries = forms.DecimalField(label='Groceries', min_value=0)
-    dining = forms.DecimalField(label='Dining out', min_value=0)
-    gas = forms.DecimalField(label='Gas', min_value=0)
-    travels = forms.DecimalField(label='Travels', min_value=0)
-    etc = forms.DecimalField(label='Everything Else', min_value=0)
-    credit_score = forms.CharField(label='What is your credit score?', widget= forms.Select(choices= CREDIT_CHOICES))
+    groceries = forms.DecimalField(label='Groceries', min_value=0, widget=forms.NumberInput(attrs={'style':'width:300px'}))
+    dining = forms.DecimalField(label='Dining out', min_value=0, widget=forms.NumberInput(attrs={'style':'width:300px'}))
+    gas = forms.DecimalField(label='Gas', min_value=0, widget=forms.NumberInput(attrs={'style':'width:300px'}))
+    travels = forms.DecimalField(label='Travels', min_value=0, widget=forms.NumberInput(attrs={'style':'width:300px'}))
+    etc = forms.DecimalField(label='Everything Else', min_value=0, widget=forms.NumberInput(attrs={'style':'width:300px'}))
+    credit_score = forms.CharField(label='What is your credit score?', widget= forms.Select(choices= CREDIT_CHOICES, attrs={'style':'width:300px'}))
     annual= forms.ChoiceField(label='Are you only interested in cards with no annual fee?', widget= forms.RadioSelect, choices= ANNUAL_CHOICES, initial= 'no')
     banks = forms.MultipleChoiceField(label= 'Show cards from the following banks:', widget= forms.CheckboxSelectMultiple, choices= BANK_CHOICES, initial= 'all')
