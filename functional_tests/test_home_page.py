91a8290bb533b69163b1a7c2f16e2df0df17086c
quetourah <dina.qd.academics@gmail.com>
2020-05-03 23:21:42 -0400

    fix redirect error

--- functional_tests/test_home_page.py
+++ functional_tests/test_home_page.py
@@ -27,7 +27,7 @@ class TestHomePage(BaseFunctionalTest):
 
     # test click Home in navbar
     def test_home_page_redirects_to_home(self):
-        self.__check_navbar_redirection_to("Home", "cards:home")
+        self.__check_navbar_redirection_to("Home\n(current)", "cards:home")
 
     # helper to test navigation bar re-directions
     def __check_navbar_redirection_to(self, element, destination):
